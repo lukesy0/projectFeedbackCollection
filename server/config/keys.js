b84d41f670b825fb1aabd12ce39ab1d0f3ff9c8f
luke <lusongyuan@lukedeMacBook-Pro.local>
2022-09-18 15:40:50 -0700

    add google authentication

---
+++ server/config/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: '610422387355-sr84fqu5i10bcpj17ru1jtg1ml7a0t48.apps.googleusercontent.com',
+    googleClientSecret: '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',
+    //mongoURI: 'mongodb://theda:obscured1@ds043605.mlab.com:43605/passport',
+    mongoURI: 'mongodb+srv://p00gz:JyKbVf7l6ATUk54h@emaily-uwsj6.mongodb.net/test?retryWrites=true',
+    cookieKey: 'jlkjfdklajfdajiejijijfdf',
+    stripePublishableKey: 'pk_test_JMdyKVvf8EGTB0Fl28GsN7YY',
+    stripeSecretKey: 'sk_test_hnfrAm8rOkryFEnV23jjfFlw',
+    sendGridKey: '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',
+    redirectDomain: 'http://localhost:5853'
+}
